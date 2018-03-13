de6a109c8bb4f68941ba07c1794bed5f557e544e
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-13 19:39:37 +0100

    Test Pug

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use('/store', function(req, res, next) {
 	console.log('I am between request and /store');
 	next();
@@ -14,7 +17,20 @@ app.get('/', function(req, res) {
 app.get('/store', function(req, res) {
 	console.log('GET for /store');
 	res.send('This is store');
-})
+});
+
+app.get('/first-template', function(req, res) {
+	res.render('first-template');
+});
+
+app.get('/dynamic-view', function(req, res) {
+	res.render('dynamic', {
+		name: 'Pug - my dynamic website',
+		url: 'http://www.google.com'/*,
+		user:
+			{name:'Jack', age:'20'}*/
+	});
+});
 
 app.listen(3000);
 
